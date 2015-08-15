4a49ee8d87ed9bc0e85f1f95eabbe87a1dccfbe0
DahuK <dahukuang@126.com>
2015-08-15 19:41:28 +0800

    scrapy tutorial

---
+++ tutorial/spiders/__init__.py
@@ -0,0 +1,19 @@
+# This package will contain the spiders of your Scrapy project
+#
+# Please refer to the documentation for information on how to create and manage
+# your spiders.
+
+
+from scrapy.spider import Spider  
+  
+class DmozSpider(Spider):  
+    name = "dmoz"  
+    allowed_domains = ["dmoz.org"]  
+    start_urls = [  
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",  
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"  
+    ]  
+  
+    def parse(self, response):  
+        filename = response.url.split("/")[-2]  
+        open(filename, 'wb').write(response.body)  
